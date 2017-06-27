72eb0e97c6f9beaac6f572022e2f8d6b859d78fb
adityadhawale <adityadhawale6@gmail.com>
2017-06-27 10:08:23 -0400

    change type 	modified:   Source/Drivers/OniFile/DataRecords.cpp

--- Source/Drivers/OniFile/DataRecords.cpp
+++ Source/Drivers/OniFile/DataRecords.cpp
@@ -664,11 +664,11 @@ XnStatus GeneralPropRecord::Decode()
 	XN_IS_STATUS_OK(nRetVal);
 
 	//The property data is not copied but just pointed to
-	XnUInt8* pData = const_cast<XnUInt8*>(GetReadPos());
+	XnUInt32* pData = const_cast<XnUInt32*>(GetReadPos());
 
 #if (XN_PLATFORM == XN_PLATFORM_LINUX_ARM || XN_PLATFORM == XN_PLATFORM_ARC || XN_PLATFORM == XN_PLATFORM_ANDROID_ARM)
 	// under ARM we have some alignment issues. Move this buffer so it will be aligned.
-	XnUInt32 nAlignFix = (XnUInt32)XN_DEFAULT_MEM_ALIGN - ((XnUInt32)pData % XN_DEFAULT_MEM_ALIGN);
+	XnUInt32 nAlignFix = XN_DEFAULT_MEM_ALIGN - ((XnUInt32)pData % XN_DEFAULT_MEM_ALIGN);
 	if (nAlignFix != 0)
 	{
 		xnOSMemMove(pData + nAlignFix, pData, m_nPropDataSize);
